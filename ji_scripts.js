ba438b8eeb8270d9966ba85149ada3b24d66eab4
bhi256@myy.haaga-helia.fi <bhi256@myy.haaga-helia.fi>
2023-10-05 20:15:08 +0300

    Sudoku background changes Navigation bar changes text highlight style element added

--- ji_scripts.js
+++ ji_scripts.js
@@ -1,6 +1,6 @@
-let index = ["home", "contact me!", "feedback", "perfect pike patty", "sudoku solver", "secret template"]
-let address = ["ji_home.html", "ji_contact.html", "ji_guest.html", "ji_project_patties.html", "ji_project_sudoku.html", "ji_project_temp.html"]
-let titles = ["Home", "Contact me!", "Feedback", "The Pike Patties", "Sudoku Solver", "Project Template"]
+let index = ["home", "contact me!", "perfect pike patty", "sudoku solver", "secret template"]
+let address = ["ji_home.html", "ji_contact.html", "ji_project_patties.html", "ji_project_sudoku.html", "ji_project_temp.html"]
+let titles = ["Home", "Contact me!", "The Pike Patties", "Sudoku Solver", "Project Template"]
 
 function searchFor(){
     let text = document.getElementById("search").value.trim().toLowerCase();
