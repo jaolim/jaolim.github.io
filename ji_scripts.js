fbe6766aa02f944330260e947aabb02ce3603946
bhi256@myy.haaga-helia.fi <bhi256@myy.haaga-helia.fi>
2023-10-05 12:58:43 +0300

    Search results added

--- ji_scripts.js
+++ ji_scripts.js
@@ -3,11 +3,19 @@ let address = ["ji_home.html", "ji_contact.html", "ji_guest.html", "ji_project_p
 
 function searchFor(){
     let text = document.getElementById("search").value.trim().toLowerCase();
-    let ref = ""
+    let results = "<h2 style=\"text-align: center\">Search Results:</h2><ul>";
+    let matches = 0;
+    let ref = "";
 
     for (let i = 0; i < index.length; i++){
         if (index[i].includes(text)){
-            location.href = address[i]
+            results = results + "<li> <a href=" + address[i] + ">" + address[i] + "</a></li>"
+            matches++;
         }
     }
+    results = results + "</ul>"
+    if (matches == 0){
+        results = "<h2 style=\"text-align: center\">Nothing found<h2>"
+    }
+    document.getElementById("results").innerHTML = results;
 }
