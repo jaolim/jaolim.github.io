fdf802e1b9d00a455bd97a96482f3842a7192e75
bhi256@myy.haaga-helia.fi <bhi256@myy.haaga-helia.fi>
2023-10-06 16:54:43 +0300

    style updates, image conversion to jpg, home background

--- ji_scripts.js
+++ ji_scripts.js
@@ -2,21 +2,33 @@ let index = ["home", "contact me!", "perfect pike patty", "sudoku solver", "secr
 let address = ["ji_home.html", "ji_contact.html", "ji_project_patties.html", "ji_project_sudoku.html", "ji_project_temp.html"]
 let titles = ["Home", "Contact me!", "The Pike Patties", "Sudoku Solver", "Project Template"]
 
-function searchFor(){
+function searchFor() {
     let text = document.getElementById("search").value.trim().toLowerCase();
-    let results = "<h2 style=\"text-align: center\">Search Results:</h2><ul>";
+    let results = "<input type=\"button\" value=\"X\" onClick=\"hideResults()\" style=\"color:red;\">" +
+        "<h2 style=\"text-align: center\">Search Results:</h2><ul style=\"padding-bottom: 0.5cm\">";
     let matches = 0;
     let ref = "";
 
-    for (let i = 0; i < index.length; i++){
-        if (index[i].includes(text)){
-            results = results + "<li> <a href=" + address[i] + ">" + titles[i] + "</a></li>"
-            matches++;
+    if (text == "") {
+        results = "<input type=\"button\" value=\"X\" onClick=\"hideResults()\" style=\"color:red;\"> <p style=\"text-align: center; padding-bottom: 0.5cm\">Please type something to search!</p>"
+        document.getElementById("results").innerHTML = results;
+        return; 
+    }
+    else {
+        for (let i = 0; i < index.length; i++) {
+            if (index[i].includes(text)) {
+                results = results + "<li> <a href=" + address[i] + ">" + titles[i] + "</a></li>"
+                matches++;
+            }
         }
     }
     results = results + "</ul>"
-    if (matches == 0){
-        results = "<h2 style=\"text-align: center\">Nothing found<h2>"
+    if (matches == 0) {
+        results = "<input type=\"button\" value=\"X\" onClick=\"hideResults()\" style=\"color:red;\"> <h2 style=\"text-align: center; padding-bottom: 0.5cm\">Nothing Found</h2>"
     }
     document.getElementById("results").innerHTML = results;
+}
+
+function hideResults() {
+    document.getElementById("results").innerHTML = "";
 }
